0f3999bbafd1f2100ce8a8d2e27d2125cfe0ced2
riyashah-easternts <riya.s.easternts@gmail.com>
2024-10-17 18:47:53 +0530

    send email after payment status - 17-10-2024

--- wp-content/themes/donatelife/ccavenue/ccavResponseHandler.php
+++ wp-content/themes/donatelife/ccavenue/ccavResponseHandler.php
@@ -30,6 +30,80 @@ $od_insert = $wpdb->insert($od_table,
 		'is_trash' => 0,
 	)
 );
+
+$admin_email = get_option('admin_email');
+$to = $admin_email;
+$subject = 'DonateLife - Online Donation from website';
+$body = do_shortcode('[email_header_template]');
+$body .= '<table class="tbl" width="80%" cellpadding="3" align="center">
+		<tbody>
+		<tr>
+		<td><strong> Name</strong></td>
+		<td>:</td>
+		<td>'.$response['billing_name'].'</td>
+		</tr>
+		<tr>
+		<td><strong> Address </strong></td>
+		<td>:</td>
+		<td>'.$response['billing_address'].'</td>
+		</tr>
+		<tr>
+		<td><strong>  City</strong></td>
+		<td>:</td>
+		<td>'.$response['billing_city'].'</td>
+		</tr>
+		<tr>
+		<td><strong>  State </strong></td>
+		<td>:</td>
+		<td>'.$response['billing_state'].'</td>
+		</tr>
+		<tr>
+		<td><strong>  Pin Code</strong></td>
+		<td>:</td>
+		<td>'.$response['billing_zip'].'</td>
+		</tr>
+		<tr>
+		<td><strong>  Country </strong></td>
+		<td>:</td>
+		<td>'.$response['billing_country'].'</td>
+		</tr>
+		<tr>
+		<td><strong>  Mobile Number </strong></td>
+		<td>:</td>
+		<td>'.$response['billing_tel'].'</td>
+		</tr>
+		<tr>
+		<td><strong>  Email Address </strong></td>
+		<td>:</td>
+		<td>'.$response['billing_email'].'</td>
+		</tr>
+		<tr>
+		<td><strong> Donation Amount </strong></td>
+		<td>:</td>
+		<td>'.$response['amount'].'</td>
+		</tr>
+		<tr>
+		<td><strong>Payment Status  </strong></td>
+		<td>:</td>
+		<td>'.$response['order_status'].'</td>
+		</tr>
+		</tbody>
+		</table>';
+	$body .= do_shortcode('[email_footer_template]');
+
+$headers = array('Content-Type: text/html; charset=UTF-8');
+
+wp_mail($to, $subject, $body, $headers);
+
+$user_email_subject = 'DonateLife - Thank you for your donation';
+$user_email = $response['billing_email'];
+$user_email_body = do_shortcode('[email_header_template]');
+$user_email_body .= '<br />Hi '.$response['billing_name'].' ,<br />Thank you for your Donation of '.$response['amount'].' INR to Donate Life Organization (An Initiative for Organ Donation). <br />';
+$user_email_body .= do_shortcode('[email_footer_template]');
+
+
+wp_mail($user_email, $user_email_subject, $user_email_body, $headers);
+
 // Check the payment status
 if ($response['order_status'] === "Success") {
 	
@@ -40,4 +114,3 @@ if ($response['order_status'] === "Success") {
     // Handle failure, log errors, etc.
 }
 ?>
-
